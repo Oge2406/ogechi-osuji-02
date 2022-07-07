28b13778c50917194de7a231c8dcb080d0064a3f
Ogechi Osuji <osujiogechi7@gmail.com>
2022-07-07 12:55:49 +0000

    1. I found the sum of numbers in an array.
    2. I counted the number of even numbers in an array

--- src/assignment.js
+++ src/assignment.js
@@ -1,3 +1,4 @@
+
 // ========================
 // DO NOT EDIT THIS BLOCK
 const assignment = {};
@@ -13,10 +14,16 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let arr = 0
+  for (let count = 0; count < arrayOfNumbers.length; count++) {
+    arr = arr + arrayOfNumbers[count]
+  } 
+  return arr
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +35,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let one = 0
+   for (let count = 0; count < arrayOfNumbers.length; count++){
+     if (arrayOfNumbers[count] % 2 == 0) {
+       one++
+     }
+   }
+ return one 
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
